8169da4818a9774b771b7c6b99b71c880f18c611
nick101992 <nick_1992@hotmail.it>
2022-11-16 11:14:26 +0100

    primo esercizio

---
+++ Esercizi scalatori.sql
@@ -0,0 +1,6 @@
+/* Query 1 - Calcolare l'insieme (non il multi-insieme) delle 
+coppie (A,B) tali che A
+è uno scalatore e B è un continente in cui A ha effettuato una scalata*/
+
+SELECT DISTINCT scalatore, nazione
+FROM Scalata
